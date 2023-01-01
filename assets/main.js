a77ea4ce5208ebd997f5e134fbe367edc855ccf9
Anshul Tomar <108689719+anshultomar07@users.noreply.github.com>
2023-01-02 00:23:03 +0530

    Added folder of website

---
+++ assets/main.js
@@ -0,0 +1,13 @@
+// let temp = document.getElementById('H')
+
+// content1 = "<span>Hi</span>"
+// content2 = "<span>H</span>"
+
+// temp.addEventListener("mouseenter", function run(){
+//     console.log("ok")
+//     temp.innerHTML = content1;
+// })
+// temp.addEventListener("mouseout", function run(){
+//     console.log("ok")
+//     temp.innerHTML = content2;
+// })
